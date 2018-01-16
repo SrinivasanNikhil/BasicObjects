0e33be1fc6c519e43d9602e4eddf667596830797
SrinivasanNikhil <nikhil.srinivasan@gmail.com>
2018-01-16 14:34:10 -0500

    TicketMachine and Printer

--- src/TicketMachine.java
+++ src/TicketMachine.java
@@ -1,21 +1,27 @@
 
 public class TicketMachine {
 
-	int nosPrinted;
+	
+	
+	public TicketMachine()
+	{
+		
+	}
+	
+	public TicketMachine(String name)
+	{
+		System.out.println("Ticket for " + name);
+		printTicket();
+	
+	}
 	
 	public void printTicket() {
 		System.out.println("***************************");
 		System.out.println("******* MARTA Ticket ******");
 		System.out.println("**** Ticket price: 10 *****");
 		System.out.println("******** Red Line *********");
-		System.out.println("***************** *********");
-		nosPrinted = nosPrinted +1;
+		System.out.println("***************************");
 	}
 	
-	public void printNosSold()
-	{
-		System.out.println("The number of tickets printed : " + nosPrinted);
-		
-	}
 
 }
--- src/TicketPrinter.java
+++ src/TicketPrinter.java
@@ -10,13 +10,49 @@ public class TicketPrinter {
 	 *
 	 * Class defined the object, like template, blueprint, it has a definition or a specific structure
 	 * 
+	 * variables can be declared and initialized in the same line
+	 * = is assignment and assigns the variable a value
+	 * 
+	 * Variables can change, but change systematically based on what was in them previously!
+	 * LHS gets the sum of collections and price, its the old value in collections, which was 0 and 
+	 * price which was 10
+	 * 
+	 * names for variables can't be reserved words, like public, private...etc...
+	 * Java supports basic arithmetic operations, PEDMAS
+	 * 
+	 * Line comment and block comment
+	 * 
+	 * Calling methods, methods can only be called on objects that they belong to. methods are public or private, 
+	 * public methods can be called by other objects to get an object to do something
+	 * 
+	 * methods tend to be public, some methods take information/data called parameters or arguments
+	 * to the method, sometimes methods make take more than one piece of information
+	 * 
+	 *  Methods can sometimes return information as well, a method can only return
+	 *  a single value or no value (void). It is part of method declaration.
+	 *  
+	 *  How are objects made? what makes the object and sets it up? the constructor
+	 *  constructor is what is involved when new is used, constructor is like a special method
+	 *  objects stored in a variable/reference variable. 
+	 *  
+	 *  
+	 *  
+	 *  
+	 * 
 	 * 
 	 *
 	 */
 	public static void main(String[] args) {
 	
-		TicketMachine tm = new TicketMachine();
+		int price = 10;
+		int collections = 0;
+		int nosPrinted = 0;
+		
+
+		TicketMachine tm = new TicketMachine(); //new keyword allows for an object to be made of a specific type
 		tm.printTicket();
+		
+		collections = collections + price;
 		
 
 	}
